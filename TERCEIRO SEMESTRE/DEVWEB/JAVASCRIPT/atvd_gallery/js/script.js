861e1de1f7e5e977780f44f322a9b7950106b3d0
gxjosue7 <gxjosue7@gmail.com>
2026-05-20 10:32:08 -0300

    iniciando atvd_gallery, estilizando style.css

---
+++ TERCEIRO SEMESTRE/DEVWEB/JAVASCRIPT/atvd_gallery/js/script.js
@@ -0,0 +1,9 @@
+const imgDisplay = document.getElementById("ImgDisplay");
+
+const miniaturas = document.querySelectorAll(".imgMini img");
+
+miniaturas.forEach(function (img) {
+  img.addEventListener("click", function () {
+    imgDisplay.src = img.src;
+  });
+});
