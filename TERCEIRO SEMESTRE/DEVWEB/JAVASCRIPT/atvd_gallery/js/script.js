95669b61a3fff6292eaf565eafd0c13075c80646
gxjosue7 <gxjosue7@gmail.com>
2026-05-20 19:48:54 -0300

    finalizado atividade gallery de devweb

--- TERCEIRO SEMESTRE/DEVWEB/JAVASCRIPT/atvd_gallery/js/script.js
+++ TERCEIRO SEMESTRE/DEVWEB/JAVASCRIPT/atvd_gallery/js/script.js
@@ -5,6 +5,9 @@ const miniaturas = document.querySelectorAll(".imgMini img");
 miniaturas.forEach(function (img) {
   img.addEventListener("click", function () {
     imgDisplay.src = img.src;
+    document.getElementById('legenda').textContent = img.alt;
   });
 });
 
+imgDisplay.src = miniaturas[0].src;
+document.getElementById('legenda').textContent = miniaturas[0].alt;
